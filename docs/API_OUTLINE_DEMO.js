c860fd1cdc8edc201b71e343c3c01820fd606346
oisin-t-ucd <oisin.tohak.ucd@gmail.com>
2026-08-20 19:41:37 +0100

    chore: add docs, implement doc 02

---
+++ docs/API_OUTLINE_DEMO.js
@@ -0,0 +1,86 @@
+// RETRIEVE A LIST OF courseS
+// HTTP GET "myapi.com/courses"
+const courses = [
+  {
+    coursename: "john",
+    age: 100,
+    is_admin: true,
+    address: null,
+    first_name: "john",
+    last_name: "smith",
+  },
+
+  {
+    coursename: "john",
+    first_name: "john",
+    last_name: "smith",
+  },
+];
+
+
+// CREATE NEW course
+// HTTP POST "myapi.com/courses"
+
+course = {
+  coursename: "john",
+  age: 100,
+  is_admin: true,
+  address: null,
+  first_name: "john",
+  last_name: "smith",
+};
+
+
+
+// RETRIEVE A SINGLE course
+// HTTP GET "myapi.com/courses/{id}"
+
+course = {
+  coursename: "john",
+  age: 100,
+  is_admin: true,
+  address: null,
+  first_name: "john",
+  last_name: "smith",
+};
+
+// EDIT course
+// HTTP PUT/PATCH "myapi.com/courses/{id}"
+
+course = {
+  coursename: "john",
+  age: 100,
+  is_admin: true,
+  address: null,
+  first_name: "john",
+  last_name: "smith",
+};
+
+// DELETE course
+// HTTP DELETE "myapi.com/courses/{id}"
+
+// CREATE COURSE
+// HTTP POST "myapi.com/courses"
+let course = {
+  title: "asdfasdfasdfasdf",
+  description: "asdfasdfasdfasdfasf",
+  is_active: true,
+};
+
+
+
+// RETRIEVE A LIST OF POSTS
+// HTTP GET "myapi.com/posts"
+
+const posts = [
+  {
+    title: "test",
+
+    description: "description",
+  },
+  {
+    title: "test",
+
+    description: "description",
+  },
+];
